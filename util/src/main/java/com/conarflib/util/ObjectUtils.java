2d5d381ed742ae88d9c2a12f36e976caabe9407c
alciran <alciran.franco@medcloud.co>
2023-03-13 19:34:13 -0300

    [ADD] Create methos do check/required object is null

--- util/src/main/java/com/conarflib/util/ObjectUtils.java
+++ util/src/main/java/com/conarflib/util/ObjectUtils.java
@@ -1,6 +1,7 @@
 package com.conarflib.util;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
@@ -31,6 +32,33 @@ public class ObjectUtils {
         } else {
             return object instanceof Map ? ((Map<?, ?>) object).isEmpty() : false;
         }
+    }
+
+    public static boolean isNull(Object object){
+        return object == null;
+    }
+
+    public static void requiredNonNull(Object object){
+        requiredNonNull(object, null);
+    }
+
+    public static void requiredNonNull(Object object, String message){
+        if(object == null)
+            throw new NullPointerException(message);
+    }
+
+    public static void requiredStringNonNull(String... strings){
+        Arrays.stream(strings).forEach(value -> {
+            if(value == null)
+                throw new NullPointerException();
+        });
+    }   
+
+    public static void requiredStringNonNullAndNonEmpty(String... strings){
+        Arrays.stream(strings).forEach(value -> {
+            if(value == null || value.isEmpty())
+                throw new NullPointerException();
+        });
     }
 
 }
